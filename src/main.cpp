ad60ca70f20e9a4bdfd89c6d3b8de3e5fdaba505
John Pugliesi <jpugliesi12@gmail.com>
2014-09-23 09:50:22 -0700

    Add test cases for maps, with assignment

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include "../lib/Set.h"
 #include "../lib/Map.h"
+#include "../lib/NoSuchElementException.h"
 
 int main(){
 
+  std::cout << "------- Sets -------" << std::endl << std::endl;
   Set<int> s1, s2;
   //Add 1-9 to set s1
   for(int i = 0; i < 10; i++){
@@ -22,6 +24,41 @@ int main(){
   for(int i = 0; i < 20; i++){
     std::cout << "S1 contains " << i << ": " << s1.contains(i) << std::endl;
   }
+
+  std::cout << "------- Maps -------" << std::endl << std::endl;
+  Map<int, int> m1, m2;
+
+  //Add 1-9 to set m1
+  for(int i = 0; i < 10; i++){
+    m1.add(i, i);
+    try{
+      std::cout << "M1 key: " << i << " value: " << m1.get(i) << std::endl;
+    } catch (NoSuchElementException &e){
+      std::cout << e.what() << std::endl;
+    }
+  }
+
+  for(int i = 11; i < 20; i++){
+    m2.add(i, i);
+    try{
+      std::cout << "M2 key: " << i << " value: " << m2.get(i) << std::endl;
+    } catch (NoSuchElementException &e){
+      std::cout << e.what() << std::endl;
+    }
+
+  }
+
+  m1 = m2;
+
+  for(int i = 0; i < 20; i++){
+    try{
+      std::cout << "M1 key: " << i << " value: " << m1.get(i) << std::endl;
+    } catch (NoSuchElementException &e){
+      std::cout << e.what() << std::endl;
+    }
+
+  }
+
 
   return 0;
 }
