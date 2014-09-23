03238d57512f4a15fc46398e6606f06554c2e161
John Pugliesi <jpugliesi12@gmail.com>
2014-09-23 09:41:03 -0700

    Add basic unit test main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "../lib/Set.h"
+#include "../lib/Map.h"
+
+int main(){
+
+  Set<int> s1, s2;
+  //Add 1-9 to set s1
+  for(int i = 0; i < 10; i++){
+    s1.add(i);
+    std::cout << "S1 contains " << i << ": " << s1.contains(i) << std::endl;
+  }
+
+  for(int i = 11; i < 20; i++){
+    s2.add(i);
+    std::cout << "S2 contains " << i << ": " << s2.contains(i) << std::endl;
+  }
+
+  for(int i = 0; i < 20; i++){
+    std::cout << "S1 contains " << i << ": " << s1.contains(i) << std::endl;
+  }
+
+  return 0;
+}
