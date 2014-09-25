8087b8476ea3e3c8ad46c157fa772d380c3182cb
John Pugliesi <jpugliesi12@gmail.com>
2014-09-24 20:19:38 -0700

    Add global variables for users, movies, and movies_by_keyword to main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 #include "lib/Set.h"
 #include "lib/Map.h"
 #include "lib/Movie.cpp"
 #include "lib/User.cpp"
 #include "lib/NoSuchElementException.h"
 
+Map<std::string, User*> users;
+Map<std::string, Movie*> movies;
+Map<std::string, Set<Movie*> movies_by_keyword;
+
 int main(int argc, char ** argv){
 
+  //Check if user provides us with the main data file
+  if (argc < 2){
+    std::cout << "USAGE: " << argv[0] << " <MAIN_DATA_FILE>" << std::endl;
+    return 1;
+  }
+  
+  
   
   return 0;
 
