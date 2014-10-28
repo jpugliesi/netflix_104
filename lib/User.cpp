b60104d61bfaf234ffb44df26f9c6f3acfa651c4
John Pugliesi <jpugliesi12@gmail.com>
2014-10-28 10:36:26 -0700

    Update User class to have movieQueue functions and current_movie

--- lib/User.cpp
+++ lib/User.cpp
@@ -5,6 +5,7 @@ While multiple users may have the same name, the ID should be unique
 User::User(std::string ID, std::string name){
   this->name = name;
   this->id = ID;
+  this->current_movie = NULL;
 }
 
 // copy constructor  
@@ -26,3 +27,41 @@ std::string User::getID() const{
 std::string User::getName() const{
   return name;
 }
+
+Queue<Movie*> * User::movieQueue () {
+
+  return &movie_queue;
+
+}
+/* returns a pointer to the user's movie queue.
+   This is the easiest way to ensure that you can modify the queue.
+   (If you return by reference, that's also possible, but you'd need
+   to be very careful not to invoke any deep copy constructors or
+   assignments. A pointer is thus safer.) */
+
+void User::rentMovie (Movie *m){
+  if (current_movie == NULL){
+    current_movie = m;
+  } else {
+    std::cout << "You already have a movie checked out. Return it before renting another." << std::endl;
+  }
+}
+  /* sets the user's currently rented movie to m.
+     If the user already has a movie checked out, then it does nothing.
+     (So no overwriting the current movie.) */
+
+void User::returnMovie (){
+
+  if (current_movie != NULL) current_movie = NULL;
+
+}
+  /* returns the movie that the user currently has checked out.
+     Does nothing if the user doesn't currently have a movie. */
+
+Movie* User::currentMovie (){
+
+  return current_movie;
+
+}
+  /* returns the user's current checked out movie.
+     Returns NULL if the user has no movie checked out. */
--- lib/User.h
+++ lib/User.h
@@ -2,6 +2,8 @@
 #define USER_H
 
 #include <string>
+#include "Queue.h"
+#include "Movie.h"
 
 class User {
   public:
@@ -17,8 +19,30 @@ class User {
     std::string getID () const;    // returns the ID associated with this user
 
     std::string getName () const;  // returns the name associated with this user
+    
+    Queue<Movie*> * movieQueue (); 
+      /* returns a pointer to the user's movie queue.
+         This is the easiest way to ensure that you can modify the queue.
+         (If you return by reference, that's also possible, but you'd need
+         to be very careful not to invoke any deep copy constructors or
+         assignments. A pointer is thus safer.) */
+
+    void rentMovie (Movie *m);
+      /* sets the user's currently rented movie to m.
+         If the user already has a movie checked out, then it does nothing.
+         (So no overwriting the current movie.) */
+
+    void returnMovie ();
+      /* returns the movie that the user currently has checked out.
+         Does nothing if the user doesn't currently have a movie. */
+
+    Movie* currentMovie ();
+      /* returns the user's current checked out movie.
+         Returns NULL if the user has no movie checked out. */
 
   private:
+    Queue<Movie*> movie_queue;
+    Movie* current_movie;
     std::string name;
     std::string id;
     
