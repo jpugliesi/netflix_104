22eee3ef416ac2b1d0b29fa030c7749ea6b8a71e
John Pugliesi <jpugliesi12@gmail.com>
2014-11-03 23:39:44 -0800

    Create Basic Main Window

---
+++ lib/MainWindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui>
+class MainWindow : public QWidget{
+
+  Q_OBJECT
+
+  public:
+    MainWindow();
+
+  private:
+    
+    QVBoxLayout *mainLayout;
+    QLabel *welcomeLabel;
+
+    QVBoxLayout *movieVBox;
+    QGroupBox *currentMovieGroup;
+    QLabel *currentMovie;
+
+    QVBoxLayout *queueVBox;
+    QGroupBox *movieQueueGroup;
+    QLabel *topOfQueue;
+
+    QVBoxLayout *searchVBox;
+    QGroupBox *searchMovieGroup;
+    QFormLayout *searchForm;
+    QLineEdit *searchText;
+    
+    QPushButton *returnMovieButton;
+    QPushButton *rentMovieButton;
+    QPushButton *removeFromQueueButton;
+    QPushButton *moveToBackOfQueueButton;
+    QPushButton *searchByTitleButton;
+    QPushButton *searchByKeywordButton;
+    QPushButton *logoutButton;
+
+
+};
+
+#endif
